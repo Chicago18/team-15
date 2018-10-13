1c3c536c6f1d10657cbc32de96d614dead8ab87a
yoni <yoniifriedman@gmail.com>
2018-10-13 03:51:43 -0500

    Generated audio translation for form questions

--- cmaaforgood/text_to_speech.py
+++ cmaaforgood/text_to_speech.py
@@ -2,30 +2,67 @@
 Note: ssml must be well-formed according to:
     https://www.w3.org/TR/speech-synthesis/
 """
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=os.getcwd() + '/config.json'
 from google.cloud import texttospeech
+
+form_questions = ['Agency Name',
+'Project Name',
+'Participant Last Name',
+'Participant First Name',
+'Participant Middle Initial',
+'Address Number',
+'Address Direction',
+'Address Street Name',
+'Address Apartment Number',
+'Type of Program',
+'Telephone Number',
+'Zip Code ',
+'Is the participant a homeless youth?',
+'Ethnicity',
+'Race',
+'Gender', 
+'Age',
+'Birth date',
+'Current Grade (If in school) or Highest Level of Education Completed',
+'School',
+'Is the participant disabled?',
+'If you answered yes to the previous question, please specify',
+'Community Area',
+'Ward',
+'Family Type',
+'Housing Status',
+'Food Stamps?',
+'Free/Reduced Lunch?',
+'Health Insurance?',
+'Income Source (Check all that apply)',
+'Source of Referral',
+'Client ID']
 
 # Instantiates a client
 client = texttospeech.TextToSpeechClient()
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text="Hello, World!")
+i = 0
+for i in range(len(form_questions)):
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.types.SynthesisInput(text=form_questions[i])
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+        # Build the voice request, select the language code ("en-US") and the ssml
+        # voice gender ("neutral")
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='es-ES',
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.types.AudioConfig(
-    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
-# The response's audio_content is binary.
-with open('output.mp3', 'wb') as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+        # The response's audio_content is binary.
+        with open('Assets/audio/question_'+str(i)+'.mp3', 'wb') as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "output'+str(i)+'.mp3"')
