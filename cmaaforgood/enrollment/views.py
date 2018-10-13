3d63e6112dcbd99e665ef617c38fd14eb7cfcb6a
yoni <yoniifriedman@gmail.com>
2018-10-13 01:17:37 -0500

    saving for later

--- cmaaforgood/enrollment/views.py
+++ cmaaforgood/enrollment/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from oauth2client.client import SignedJwtAssertionCredentials
 from __future__ import unicode_literals
+from __future import print_function
+import gspread
+import pandas as pd
+import json
 
 from django.shortcuts import render
 
-# Create your views here.
+def index():
+        return HttpResponse("Hello World")
