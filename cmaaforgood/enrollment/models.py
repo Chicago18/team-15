ec3dbe71f856681481403bd0469b30fb5f4b2ccc
yoni <yoniifriedman@gmail.com>
2018-10-13 07:52:15 -0500

    Created ORM

--- cmaaforgood/enrollment/models.py
+++ cmaaforgood/enrollment/models.py
@@ -10,8 +10,7 @@ class Parent(models.Model):
     p2_firstname = models.CharField(max_length=45)
     p2_lastname = models.CharField(max_length=45)
     p1_telephone = models.CharField(max_length=45)
-    p2_telephone = models.CharField(max_length=45)
-    
+     
     class Meta:
             unique_together = ('p1_firstname', 'p1_lastname', 'p1_telephone')
 
