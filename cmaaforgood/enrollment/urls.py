bdc8f60af4fc68225acb09045c036178be998e3f
yoni <yoniifriedman@gmail.com>
2018-10-13 10:01:53 -0500

    everything is burning send help

--- cmaaforgood/enrollment/urls.py
+++ cmaaforgood/enrollment/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.conf.urls import url, include 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/$', views.login, name='login'),
-    path('profile/(?P<username>\w+)/$', views.profile, name='profile'),
-    path('parentHome/$', views.parentHome, name='parentHome'),
-    path('parentLogin/$', views.parentLogin, name='parentLogin'),
-    path('childProfile/$', views.childProfile, name='profile'),
-    path('forms/$', views.forms, name='forms'),
-    path('enroll/$', views.enroll, name='enroll'),
+    url('', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^profile/(?P<username>\w+)/$', views.profile, name='profile'),
+    url(r'^parentHome/$', views.parentHome, name='parentHome'),
+    url(r'^parentLogin/$', views.parentLogin, name='parentLogin'),
+    url(r'^childProfile/$', views.childProfile, name='profile'),
+    url(r'^forms/$', views.forms, name='forms'),
+    url(r'^enroll/$', views.enroll, name='enroll'),
     ]
--- cmaaforgood/enrollment/views.py
+++ cmaaforgood/enrollment/views.py
@@ -3,6 +3,45 @@ from __future__ import unicode_literals
 
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
-from django.models import Parents, Child, Assignments, Users
+from django.http import Http404
+from enrollment.models import Parent, Child, Assignments, Users
 
 
+def index(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def login(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def profile(request,username=""):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def parentHome(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def parentLogin(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def childProfile(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def forms(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
+
+
+def enroll(request):
+    if request.method == 'GET':
+            return render(request, 'enrollment/home.html', {})
