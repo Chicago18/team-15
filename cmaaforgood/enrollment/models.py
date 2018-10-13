57f24829a315ccac0a97e6f6026bd42b750e9d98
yoni <yoniifriedman@gmail.com>
2018-10-13 09:26:22 -0500

    branch for last minute fires

--- cmaaforgood/enrollment/models.py
+++ cmaaforgood/enrollment/models.py
@@ -8,41 +8,41 @@ class Parent(models.Model):
     p2_firstname = models.CharField(max_length=45)
     p2_lastname = models.CharField(max_length=45)
     p1_telephone = models.CharField(max_length=45)
-    p1_address = model.CharField(max_length=45) 
+    p1_address = models.CharField(max_length=45) 
 
 
 class Child(models.Model):
-    fname = model.CharField(max_length=45) 
-    lname = model.CharField(max_length=45) 
-    grade = model.CharField(max_length=45) 
-    username = model.CharField(max_length=45) 
-    p1_telephone = model.CharField(max_length=45) 
-    p1_fname = model.CharField(max_length=45) 
-    p1_lname = model.CharField(max_length=45) 
-    p2_fname = model.CharField(max_length=45) 
-    p2_lname = model.CharField(max_length=45)
+    fname = models.CharField(max_length=45) 
+    lname = models.CharField(max_length=45) 
+    grade = models.CharField(max_length=45) 
+    username = models.CharField(max_length=45) 
+    p1_telephone = models.CharField(max_length=45) 
+    p1_fname = models.CharField(max_length=45) 
+    p1_lname = models.CharField(max_length=45) 
+    p2_fname = models.CharField(max_length=45) 
+    p2_lname = models.CharField(max_length=45)
     
 
 class Enrollment(models.Model):
-        ch_fname = model.CharField(max_length=45) 
-        ch_lname = model.CharField(max_length=45) 
-        p1_tel = model.CharField(max_length=45) 
-        p1_fname = model.CharField(max_length=45) 
-        p2_fname = model.CharField(max_length=45) 
-        p1_lname = model.CharField(max_length=45) 
-        p2_lname = model.CharField(max_length=45)  
+        ch_fname = models.CharField(max_length=45) 
+        ch_lname = models.CharField(max_length=45) 
+        p1_tel = models.CharField(max_length=45) 
+        p1_fname = models.CharField(max_length=45) 
+        p2_fname = models.CharField(max_length=45) 
+        p1_lname = models.CharField(max_length=45) 
+        p2_lname = models.CharField(max_length=45)  
 
 
 class Users(models.Model):
-        uname = model.CharField(max_length=45) 
-        pword = model.CharField(max_length=45) 
-        pnum = model.CharField(max_length=45)  
+        uname = models.CharField(max_length=45) 
+        pword = models.CharField(max_length=45) 
+        pnum = models.CharField(max_length=45)  
 
 class Assignments(models.Model):
-        topic = model.CharField(max_length=45)  
-        difficulty = model.CharField(max_length=45)
-        level = model.CharField(max_length=45)
-        question = model.CharField(max_length=500)
+        topic = models.CharField(max_length=45)  
+        difficulty = models.CharField(max_length=45)
+        level = models.CharField(max_length=45)
+        question = models.CharField(max_length=500)
 
  
 
--- cmaaforgood/enrollment/views.py
+++ cmaaforgood/enrollment/views.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
-from django.models import parents, child, assignments, users
+from django.models import Parents, Child, Assignments, Users
 
 
