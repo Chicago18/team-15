0e726e85f6c340867b0dde9902f34f37b7d72a81
yoni <yoniifriedman@gmail.com>
2018-10-13 08:10:39 -0500

    Updated ORM

--- cmaaforgood/enrollment/models.py
+++ cmaaforgood/enrollment/models.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+# -*- coding: utf-8 -*- from __future__ import unicode_literals 
 from django.db import models
 
 
@@ -10,10 +8,43 @@ class Parent(models.Model):
     p2_firstname = models.CharField(max_length=45)
     p2_lastname = models.CharField(max_length=45)
     p1_telephone = models.CharField(max_length=45)
-     
-    class Meta:
-            unique_together = ('p1_firstname', 'p1_lastname', 'p1_telephone')
+    p1_address = model.CharField(max_length=45) 
 
 
-#class Child(models.Model):
-     
+class Child(models.Model):
+    fname = model.CharField(max_length=45) 
+    lname = model.CharField(max_length=45) 
+    grade = model.CharField(max_length=45) 
+    username = model.CharField(max_length=45) 
+    p1_telephone = model.CharField(max_length=45) 
+    p1_fname = model.CharField(max_length=45) 
+    p1_lname = model.CharField(max_length=45) 
+    p2_fname = model.CharField(max_length=45) 
+    p2_lname = model.CharField(max_length=45)
+    
+
+class Enrollment(models.Model):
+        ch_fname = model.CharField(max_length=45) 
+        ch_lname = model.CharField(max_length=45) 
+        p1_tel = model.CharField(max_length=45) 
+        p1_fname = model.CharField(max_length=45) 
+        p2_fname = model.CharField(max_length=45) 
+        p1_lname = model.CharField(max_length=45) 
+        p2_lname = model.CharField(max_length=45)  
+
+
+class Users(models.Model):
+        uname = model.CharField(max_length=45) 
+        pword = model.CharField(max_length=45) 
+        pnum = model.CharField(max_length=45)  
+
+class Assignments(models.Model):
+        topic = model.CharField(max_length=45)  
+        difficulty = model.CharField(max_length=45)
+        level = model.CharField(max_length=45)
+        question = model.CharField(max_length=500)
+
+ 
+
+
+
